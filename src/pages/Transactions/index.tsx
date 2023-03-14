de5e07e6d68012eac8f186168e669db41af77bca
Patric de Paiva Fonseca <patric.ccomp@gmail.com>
2023-03-13 21:30:39 -0300

    feat: add context and json-server

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,5 +1,7 @@
+import { useContext } from "react";
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
+import { TransactionsContext } from "../../contexts/TransactionsContext";
 import { SearchForm } from "./components/SearchForm";
 import {
 	PriceHighLight,
@@ -8,6 +10,7 @@ import {
 } from "./styles";
 
 export function Transaction() {
+	const { transactions } = useContext(TransactionsContext);
 	return (
 		<>
 			<Header />
@@ -17,23 +20,20 @@ export function Transaction() {
 				<SearchForm />
 				<TransactionTable>
 					<tbody>
-						<tr>
-							<td width="50%">Desenvolvimento de site</td>
-							<td>
-								<PriceHighLight variant="income">R$ 12000,00</PriceHighLight>
-							</td>
-							<td>Venda</td>
-							<td>12/03/2022</td>
-						</tr>
-
-						<tr>
-							<td width="50%">Hamburguer</td>
-							<td>
-								<PriceHighLight variant="outcome">- R$ 30,00</PriceHighLight>
-							</td>
-							<td>Alimentação</td>
-							<td>12/03/2022</td>
-						</tr>
+						{transactions.map((transaction) => {
+							return (
+								<tr>
+									<td width="50%">{transaction.description}</td>
+									<td>
+										<PriceHighLight variant={transaction.type}>
+											{transaction.price}
+										</PriceHighLight>
+									</td>
+									<td>{transaction.category}</td>
+									<td>{transaction.createdAt}</td>
+								</tr>
+							);
+						})}
 					</tbody>
 				</TransactionTable>
 			</TransactionContainer>
