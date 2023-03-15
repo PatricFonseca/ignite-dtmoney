396f8e2178ee9683c35206bceacc76eae4b632ba
Patric de Paiva Fonseca <patric.ccomp@gmail.com>
2023-03-14 21:23:05 -0300

    feat: add react-hook-form zod and hook useSummary

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,8 +1,9 @@
 import { useContext } from "react";
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
-import { TransactionsContext } from "../../contexts/TransactionsContext";
 import { SearchForm } from "./components/SearchForm";
+import { TransactionsContext } from "../../contexts/TransactionsContext";
+import { dateFormatter, priceFormatter } from "../../utils/formatter";
 import {
 	PriceHighLight,
 	TransactionContainer,
@@ -22,15 +23,18 @@ export function Transaction() {
 					<tbody>
 						{transactions.map((transaction) => {
 							return (
-								<tr>
+								<tr key={transaction.id}>
 									<td width="50%">{transaction.description}</td>
 									<td>
 										<PriceHighLight variant={transaction.type}>
-											{transaction.price}
+											{transaction.type === "outcome" && "- "}
+											{priceFormatter.format(transaction.price)}
 										</PriceHighLight>
 									</td>
 									<td>{transaction.category}</td>
-									<td>{transaction.createdAt}</td>
+									<td>
+										{dateFormatter.format(new Date(transaction.createdAt))}
+									</td>
 								</tr>
 							);
 						})}
